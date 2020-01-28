45cd9288931468dbb77aced0e58bde83bc8a5e10
bdsqqq <igorbedesqui@gmail.com>
2020-01-28 09:00:05 -0300

    Atualização do routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Landing from './landing'
 import Contato from './contato'
+import Tcc from './tcc'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App(){
@@ -10,6 +11,7 @@ function App(){
       <Switch>
         <Route path="/" exact component={Landing} />
         <Route path="/contato" component={Contato} />
+        <Route path="/tcc" component={Tcc} />
       </Switch>
     </Router>
   );
