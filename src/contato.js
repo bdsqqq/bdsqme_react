c7ebee5ac290f49b3dff4b826e494120132013a5
bdsqqq <igorbedesqui@gmail.com>
2020-01-27 21:24:20 -0300

    Implementação de hero image na pagina

--- src/contato.js
+++ src/contato.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import HeroImage from './heroImage';
 import Nav from './navbar';
 import Footer from './footer'
 import HeroText from './heroText'
@@ -7,6 +8,7 @@ function Contato(){
 
     return(
         <>
+            <HeroImage size={70} photoURL={'https://images.unsplash.com/photo-1565207470645-0c3bff5d8c37?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'} />
             <Nav isOnlyHeader={false} />
             <HeroText title='Contato' text='Para contato profissional, por favor, mande um email para igorbedesqui@gmail.com' />
             <Footer/>
